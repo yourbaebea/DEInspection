04fe530efd50173f376898fad83f6a478e9fad5d
yourbaebea <49524805+yourbaebea@users.noreply.github.com>
2020-12-09 16:15:17 +0000

    Add files via upload

--- app/src/main/java/com/example/deinspection/NewCarActivity.kt
+++ app/src/main/java/com/example/deinspection/NewCarActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
+
 class NewCarActivity : AppCompatActivity() {
     var number: Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.create_new_car)
+
+        setContentView(R.layout.activity_new_car)
 
         var value: String? = intent.getStringExtra("CAR_NUMBER")
 
@@ -20,13 +22,18 @@ class NewCarActivity : AppCompatActivity() {
         this.number= aux
     }
 
-    fun Next(view: View) {
 
-        setContentView(R.layout.create_next)
+
+    fun nextButton(view: View) {
+        //save definitions
+        setContentView(R.layout.activity_new_car_2)
+
+
 
     }
 
-    fun nextButton(view: View) {
+
+    fun next2Button(view: View) {
         //save definitions
         val intent = Intent(this, NewCar2Activity::class.java)
         //send only check options to set definitions
@@ -36,18 +43,7 @@ class NewCarActivity : AppCompatActivity() {
 
 
 
-    fun seeCar(view: View) {
 
-        // if btnFinish is clicked
-        // check if value == 1, thats an error
-        val intent = Intent(this, CarActivity::class.java)
-        intent.putExtra(CAR_NUMBER, this.number)
-        //intent.putExtra(CAR_LIST, list)
-        // send the list of all the cars
-        startActivity(intent)
-
-
-    }
 
 
 }
