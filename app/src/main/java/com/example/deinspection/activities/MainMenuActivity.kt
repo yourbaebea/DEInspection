ec6da176d56291304203928f1ecd0b238387677c
yourbaebea <anafm@student.dei.uc.pt>
2020-12-10 13:59:33 +0000

    uwu

---
+++ app/src/main/java/com/example/deinspection/activities/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deinspection.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deinspection.R
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
