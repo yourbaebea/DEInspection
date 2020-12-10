354db0d50980b6b70a929405d3dc43a80c7dc740
yourbaebea <anafm@student.dei.uc.pt>
2020-12-10 18:47:40 +0000

    uwu

--- app/src/main/java/com/example/deinspection/activities/MainMenuActivity.kt
+++ app/src/main/java/com/example/deinspection/activities/MainMenuActivity.kt
@@ -1,27 +1,65 @@
 package com.example.deinspection.activities
 
+import android.app.TimePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.deinspection.CAR_NUMBER
 import com.example.deinspection.R
+import kotlinx.android.synthetic.main.activity_main_menu.*
+import kotlinx.android.synthetic.main.activity_settings.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainMenuActivity : AppCompatActivity() {
+    var value: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        btnCar1.setOnClickListener() {
+            seeCar(1)
+        }
+        btnCar2.setOnClickListener() {
+            seeCar(2)
+        }
+        btnCar3.setOnClickListener() {
+            seeCar(3)
+        }
+        btnCar4.setOnClickListener() {
+            seeCar(4)
+        }
+        btnCar5.setOnClickListener() {
+            seeCar(5)
+        }
+
+        btnSettings.setOnClickListener() {
+            //check if list of current cars is equal max (3?)
+            // if list of cars is max error message
+
+            //else
+            // list of cars append new empty one
+            //go to page create new car
+
+            /*
+            if (size of list of car < max)
+             */
+
+            val intent = Intent(this, NewCarActivity::class.java)
+            //value = size of list of cars
+            var value: Int = 3
+            //TEMPORARIO
+            intent.putExtra(CAR_NUMBER, value)
+            //intent.putExtra(CAR_LIST, list)
+            // send the list of all the cars, for us to add
+            startActivity(intent)
+        }
+
+
     }
 
-
-    fun seeCar(view: View) {
-
-        // if any of the btnCar is clicked
-
-        var value: Int = 0
-        if(view.id.toString() == "btnCar1") value = 1
-        if(view.id.toString() == "btnCar2") value = 2
-        if(view.id.toString() == "btnCar3") value = 3
+    fun seeCar(n: Int) {
 
         // check if value == 1, thats an error
         val intent = Intent(this, CarActivity::class.java)
@@ -29,35 +67,7 @@ class MainMenuActivity : AppCompatActivity() {
         //intent.putExtra(CAR_LIST, list)
         // send the list of all the cars
         startActivity(intent)
-
-
     }
 
-
-
-    fun addNewCar(view: View) {
-
-
-        //check if list of current cars is equal max (3?)
-        // if list of cars is max error message
-
-        //else
-        // list of cars append new empty one
-        //go to page create new car
-
-        /*
-        if (size of list of car < max)
-         */
-
-        val intent = Intent(this, NewCarActivity::class.java)
-        //value = size of list of cars
-        var value: Int= 3
-        //TEMPORARIO
-        intent.putExtra(CAR_NUMBER, value)
-        //intent.putExtra(CAR_LIST, list)
-        // send the list of all the cars, for us to add
-        startActivity(intent)
-
-
-    }
 }
+
