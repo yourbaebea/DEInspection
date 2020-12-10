bb61889324216e899cfbdd431e91096f55a11233
yourbaebea <anafm@student.dei.uc.pt>
2020-12-10 17:45:12 +0000

    uwu

--- app/src/main/java/com/example/deinspection/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/deinspection/activities/SettingsActivity.kt
@@ -9,6 +9,7 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.TimePickerDialog
 import android.graphics.Color
@@ -26,20 +27,27 @@ import java.util.Calendar
 
 
 class SettingsActivity : AppCompatActivity() {
+    //this should be a global var, default time of hour or minute
+    var hour : Int =9
+    var minute: Int =30
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        timeTv.text= hour.toString() + ":" + minute.toString() //time for alarm
+
+        //edit time for alarm
         pickTimeBtn.setOnClickListener() {
             val cal = Calendar.getInstance()
             val timeSet = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
                 cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
                 cal.set(Calendar.MINUTE, minute)
                 timeTv.text = SimpleDateFormat("HH:mm", Locale.getDefault()).format(cal.time)
-
+                hour
             }
             TimePickerDialog(this, timeSet, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
 
         }
     }
 }
+
