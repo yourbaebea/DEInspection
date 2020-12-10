699aa7798b04e9505a66c7ef4a64e29d6aa5dfd4
yourbaebea <anafm@student.dei.uc.pt>
2020-12-10 15:56:56 +0000

    uwu

---
+++ app/src/main/java/com/example/deinspection/activities/SettingsActivity.kt
@@ -0,0 +1,45 @@
+package com.example.deinspection.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deinspection.R
+import kotlinx.android.synthetic.main.activity_settings.*
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.app.TimePickerDialog
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.DecelerateInterpolator
+import androidx.core.graphics.ColorUtils
+
+
+
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Calendar
+
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        pickTimeBtn.setOnClickListener() {
+            val cal = Calendar.getInstance()
+            val timeSet = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                cal.set(Calendar.MINUTE, minute)
+                timeTv.text = SimpleDateFormat("HH:mm", Locale.getDefault()).format(cal.time)
+
+            }
+            TimePickerDialog(this, timeSet, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+
+        }
+    }
+}
