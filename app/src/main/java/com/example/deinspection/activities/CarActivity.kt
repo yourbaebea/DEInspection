e35acf036aa22faf3ff036db428c8d500527dd7f
ventura2709 <50402033+ventura2709@users.noreply.github.com>
2020-12-15 20:26:28 +0000

    seekbar done?

--- app/src/main/java/com/example/deinspection/activities/CarActivity.kt
+++ app/src/main/java/com/example/deinspection/activities/CarActivity.kt
@@ -70,4 +70,5 @@ class CarActivity : AppCompatActivity() {
 
 
 
+
 }
--- app/src/main/java/com/example/deinspection/activities/NewCar3Activity.kt
+++ app/src/main/java/com/example/deinspection/activities/NewCar3Activity.kt
@@ -2,13 +2,12 @@ package com.example.deinspection.activities
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.SeekBar
-import android.widget.Toast
+import android.widget.*
+import androidx.annotation.RequiresApi
 import com.example.deinspection.ATTRIBUTE
 import com.example.deinspection.R
 import com.example.deinspection.classes.Car
@@ -17,15 +16,83 @@ import kotlinx.android.synthetic.main.activity_new_car_3.*
 
 class NewCar3Activity : AppCompatActivity() {
     var car= Car()
+
+    lateinit var slider : SeekBar
+    lateinit var valor : TextView
+    lateinit var slider2 : SeekBar
+    lateinit var valor2 : TextView
+    lateinit var slider3 : SeekBar
+    lateinit var valor3 : TextView
+    lateinit var slider4 : SeekBar
+    lateinit var valor4 : TextView
+    lateinit var slider5 : SeekBar
+    lateinit var valor5 : TextView
+    lateinit var slider6 : SeekBar
+    lateinit var valor6 : TextView
+    lateinit var slider7 : SeekBar
+    lateinit var valor7 : TextView
+    lateinit var slider8 : SeekBar
+    lateinit var valor8 : TextView
+    lateinit var slider9 : SeekBar
+    lateinit var valor9 : TextView
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_car_3)
+
+        seekBar.min = 1
+        seekBar.max = 26
+        seekBar2.min = 1
+        seekBar2.max = 26
+        seekBar3.min = 1
+        seekBar3.max = 26
+        seekBar4.min = 1
+        seekBar4.max = 26
+        seekBar5.min = 1
+        seekBar5.max = 26
+        seekBar6.min = 1
+        seekBar6.max = 26
+        seekBar7.min = 1
+        seekBar7.max = 26
+        seekBar8.min = 1
+        seekBar8.max = 26
+        seekBar9.min = 1
+        seekBar9.max = 26
+
+        slider = findViewById(R.id.seekBar) as SeekBar
+        valor = findViewById(R.id.attribute1) as TextView
+        slider2 = findViewById(R.id.seekBar2) as SeekBar
+        valor2 = findViewById(R.id.attribute2) as TextView
+        slider3 = findViewById(R.id.seekBar3) as SeekBar
+        valor3 = findViewById(R.id.attribute3) as TextView
+        slider4 = findViewById(R.id.seekBar4) as SeekBar
+        valor4 = findViewById(R.id.attribute4) as TextView
+        slider5 = findViewById(R.id.seekBar5) as SeekBar
+        valor5 = findViewById(R.id.attribute5) as TextView
+        slider6 = findViewById(R.id.seekBar6) as SeekBar
+        valor6 = findViewById(R.id.attribute6) as TextView
+        slider7 = findViewById(R.id.seekBar7) as SeekBar
+        valor7 = findViewById(R.id.attribute7) as TextView
+        slider8 = findViewById(R.id.seekBar8) as SeekBar
+        valor8 = findViewById(R.id.attribute8) as TextView
+        slider9 = findViewById(R.id.seekBar9) as SeekBar
+        valor9 = findViewById(R.id.attribute9) as TextView
 
         // if the car already exists, we fill it with the info we already have
         // FOR THE LOVE OF GOD IGNORE THIS SIMPLIFICATION
         // O CARRO NAO VAI SER SEMPRE VAZIO ELE APENAS ESTÁ VAZIO PORQUE ELE FOI DEFINIDO NA LINHA 19
         if (car != null) filledAlready()
 
+        seekBar(slider, valor)
+        seekBar(slider2, valor2)
+        seekBar(slider3, valor3)
+        seekBar(slider4, valor4)
+        seekBar(slider5, valor5)
+        seekBar(slider6, valor6)
+        seekBar(slider7, valor7)
+        seekBar(slider8, valor8)
+        seekBar(slider9, valor9)
 
         btnBackAL.setOnClickListener() {
 
@@ -76,7 +143,7 @@ class NewCar3Activity : AppCompatActivity() {
 
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.O)
     fun filledAlready() {
 
         seekBar.progress= car.oil.reminder
@@ -93,8 +160,6 @@ class NewCar3Activity : AppCompatActivity() {
         seekBar2.progress= 1
     }
 
-
-
     //save all the info in this page
     fun saveInfo(){
         car.oil.reminder= seekBar.progress
@@ -108,17 +173,11 @@ class NewCar3Activity : AppCompatActivity() {
         car.custom2.reminder = seekBar9.progress
     }
 
-
     //where the back button goes to
     fun goBack(){
         val intent = Intent(this, NewCar2Activity::class.java)
         startActivity(intent)
     }
-
-
-
-
-
 
     fun editTV(value: Int): String {
         val aux: Int
@@ -147,14 +206,23 @@ class NewCar3Activity : AppCompatActivity() {
             else -> println("something went wrong value is out of bounds")
         }
         return str
-
     }
 
+    fun seekBar(a : SeekBar, b : TextView){
+        a.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                b.text = editTV(a.progress)
+            }
 
-    fun seekBar(){
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
 
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
 
-
+    /*fun seekBar(){
 
         // defining SeekBar
         val seek = SeekBar(this)
@@ -188,7 +256,7 @@ class NewCar3Activity : AppCompatActivity() {
                     // write custom code when touch is stopped
                 }
             })
-    }
+    }*/
 
 /*
 
